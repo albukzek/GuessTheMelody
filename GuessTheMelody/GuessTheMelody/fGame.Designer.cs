025f9c26c49096f9ea7102130a6a10b854a86d4a
Вадим Трегубенко <albukzek@gmail.com>
2019-03-08 00:34:08 +0300

    1

---
+++ GuessTheMelody/GuessTheMelody/fGame.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace GuessTheMelody
+{
+    partial class fGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fGame));
+            this.WMP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.bNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WMP
+            // 
+            this.WMP.Enabled = true;
+            this.WMP.Location = new System.Drawing.Point(79, 86);
+            this.WMP.Name = "WMP";
+            this.WMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP.OcxState")));
+            this.WMP.Size = new System.Drawing.Size(75, 23);
+            this.WMP.TabIndex = 0;
+            this.WMP.Visible = false;
+            // 
+            // bNext
+            // 
+            this.bNext.Location = new System.Drawing.Point(199, 117);
+            this.bNext.Name = "bNext";
+            this.bNext.Size = new System.Drawing.Size(133, 89);
+            this.bNext.TabIndex = 1;
+            this.bNext.Text = "Следующая";
+            this.bNext.UseVisualStyleBackColor = true;
+            this.bNext.Click += new System.EventHandler(this.bNext_Click);
+            // 
+            // fGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 385);
+            this.Controls.Add(this.bNext);
+            this.Controls.Add(this.WMP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fGame";
+            this.Text = "Игра";
+            ((System.ComponentModel.ISupportInitialize)(this.WMP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer WMP;
+        private System.Windows.Forms.Button bNext;
+    }
+}
---
+++ GuessTheMelody/GuessTheMelody/fGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheMelody
+{
+    public partial class fGame : Form
+    {
+        Random rnd = new Random();
+        public fGame()
+        {
+            InitializeComponent();
+        }
+
+        private void bNext_Click(object sender, EventArgs e)
+        {
+            int x = rnd.Next(0, Victorina.msc.Count());
+            WMP.URL = Victorina.msc[x];
+        }
+    }
+}
